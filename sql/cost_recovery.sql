97681ebc443312ba6813c775fa15a7d7272a9bb9
Iain-S <25081046+Iain-S@users.noreply.github.com>
2023-11-17 15:56:53 +0000

    Update integrity checks and schema name

--- sql/cost_recovery.sql
+++ sql/cost_recovery.sql
@@ -14,7 +14,7 @@ join (
     on latest_names.subscription_id = cr.subscription_id
 join accounting.subscription_details sd
     on sd.id = latest_names.max_id
-where cr.month >= '2022-04-00';
+where cr.month >= '2022-04-00'; -- the first day of the recovery period
 
 
 -- Export Query 2
@@ -75,40 +75,15 @@ and f.date_from < '2023-01-00';  -- one day after the recovery-period end date
 
 
 -- Data Integrity Query 1
--- Costly subscriptions without cost recovery
--- These could simply be core-funded but worth checking those over ~£500
-select sum(u.total_cost), u.subscription_name, date_trunc('month', u.date) themonth
-from ea."usage" u
-where (u.subscription_id, date_trunc('month', u.date)) in (
-  select * from (
-    select distinct ub.subscription_id subid, date_trunc('month', ub.date) themonth
-    from ea.usage ub
-    where date_trunc('month', ub.date) >= '2023-04-00'
-    and date_trunc('month', ub.date) < '2023-07-00'
-  ) a
-  where (a.subid, a.themonth) not in (
-    select cr.subscription_id, cr."month"
-    from ea.cost_recovery cr
-    where cr."month" >= '2023-04-00'
-    and cr."month" < '2023-07-00'
-  )
-)
-and date_trunc('month', u.date) >= '2023-04-00'
-and date_trunc('month', u.date) < '2023-07-00'
-group by u.subscription_name, date_trunc('month', u.date)
-order by sum(u.total_cost) desc, date_trunc('month', u.date) asc;
-
-
--- Data Integrity Query 2
 -- Date-from and date-to mismatches between approval and finance tables
 -- We expect some mismatches but it is easy to be off by a month or a year
 WITH fin_tbl AS (
     SELECT subscription_id, ticket, amount, date_from, date_to
-    FROM ea.finance
+    FROM accounting.finance
 ),
 app_tbl AS (
     SELECT subscription_id, ticket, amount, date_from, date_to
-    FROM ea.approvals
+    FROM accounting.approvals
 ),
 names as (
 	select
@@ -116,21 +91,21 @@ names as (
 	    sd.display_name
 	from (
 	    select max("id") as max_id, subscription_id
-		from ea.subscription_details sd
+		from accounting.subscription_details sd
 		group by subscription_id
 	) latest_names
-	join ea.subscription_details sd
+	join accounting.subscription_details sd
 	    on sd.id = latest_names.max_id
 )
 select
     n.display_name,
     app_tbl.subscription_id,
     app_tbl.ticket,
-    app_tbl.date_from,
-    fin_tbl.date_from,
+    app_tbl.date_from as "app_date_from",
+    fin_tbl.date_from as "fin_date_from",
     fin_tbl.date_from - app_tbl.date_from as "startdiff",
-    app_tbl.date_to,
-    fin_tbl.date_to,
+    app_tbl.date_to as "app_date_to",
+    fin_tbl.date_to as "fin_date_to",
     fin_tbl.date_to - app_tbl.date_to as "enddiff"
 FROM fin_tbl
 JOIN app_tbl
@@ -138,11 +113,12 @@ JOIN app_tbl
     AND fin_tbl.ticket = app_tbl.ticket
 join names n
     on n.subscription_id = fin_tbl.subscription_id
-where app_tbl.date_to > '2023-04-00'
+where app_tbl.date_to > '2023-04-00'  -- First day of the financial year
+-- and app_tbl.date_from > '2023-07-00' -- you may wish to limit to the recovery period
 order by n.display_name asc;
 
 
--- Data Integrity Query 3
+-- Data Integrity Query 2
 -- Approvals without a matching finance row
 WITH names as (
 	select
@@ -150,28 +126,59 @@ WITH names as (
 	    sd.display_name
 	from (
 	    select max("id") as max_id, subscription_id
-		from ea.subscription_details sd
+		from accounting.subscription_details sd
 		group by subscription_id
 	) latest_names
-	join ea.subscription_details sd
+	join accounting.subscription_details sd
 	    on sd.id = latest_names.max_id
 )
 select n.display_name, a.*
-from ea.approvals a
+from accounting.approvals a
 join names n
   on n.subscription_id = a.subscription_id
 where not exists (
   select 1
-  from ea.finance f
+  from accounting.finance f
   where f.ticket like (a.ticket || '%')
 )
-and a.date_to > '2023-04-00';
+and a.date_to > '2023-04-00' -- First day of the financial year
+-- and a.date_from > '2023-07-00' -- you may wish to limit to the recovery period
+order by n.display_name asc;
 
--- Data Integrity Query 4
+
+-- Data Integrity Query 3
 -- Finance without a matching approvals row
 select *
-from ea.finance f
+from accounting.finance f
 where f.ticket not in (
   select distinct a.ticket
-  from ea.approvals a
-);
+  from accounting.approvals a
+)
+and f.date_to >= '2023-07-01' -- first day of the recovery period
+order by f.date_to asc;
+
+
+-- Data Integrity Query 4
+-- !!Note this should be run after the cost_recovery table has been updated!!
+-- Costly subscriptions without cost recovery
+-- These could simply be core-funded but worth checking those over ~£500
+select sum(u.total_cost), u.subscription_name, date_trunc('month', u.date) themonth
+from accounting."usage" u
+where (u.subscription_id, date_trunc('month', u.date)) in (
+  select * from (
+    select distinct ub.subscription_id subid, date_trunc('month', ub.date) themonth
+    from accounting.usage ub
+    where date_trunc('month', ub.date) >= '2023-04-00'  -- the recovery-period start date
+    and date_trunc('month', ub.date) < '2023-07-00'  -- one day after the recovery-period end date
+  ) a
+  where (a.subid, a.themonth) not in (
+    select cr.subscription_id, cr."month"
+    from accounting.cost_recovery cr
+    where cr."month" >= '2023-04-00'  -- the recovery-period start date
+    and cr."month" < '2023-07-00'  -- one day after the recovery-period end date
+  )
+)
+and date_trunc('month', u.date) >= '2023-04-00'  -- the recovery-period start date
+and date_trunc('month', u.date) < '2023-07-00'  -- one day after the recovery-period end date
+group by u.subscription_name, date_trunc('month', u.date)
+order by sum(u.total_cost) desc, date_trunc('month', u.date) asc;
