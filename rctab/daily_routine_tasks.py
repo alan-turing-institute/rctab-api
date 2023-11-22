24cdca798eeb0912dbae4d05054eec30c5ca3c07
Iain-S <25081046+Iain-S@users.noreply.github.com>
2023-11-22 15:31:52 +0000

    Remove unused routine tasks code

--- rctab/daily_routine_tasks.py
+++ rctab/daily_routine_tasks.py
@@ -1,59 +1,21 @@
 """Background tasks that run daily."""
-import contextlib
 import logging
-from asyncio import sleep
-from datetime import datetime, timedelta, timezone
-from pathlib import Path
-from types import TracebackType
-from typing import Final, List, Optional, Type
+from datetime import datetime, timezone
+from typing import List, Optional
 from uuid import UUID
 
 from sqlalchemy import desc, insert, select
 
-from rctab.constants import ADMIN_OID, EMAIL_TYPE_SUMMARY
+from rctab.constants import EMAIL_TYPE_SUMMARY
 from rctab.crud.accounting_models import emails, failed_emails
 from rctab.crud.models import database
-from rctab.routers.accounting.abolishment import abolish_subscriptions
 from rctab.routers.accounting.send_emails import (
     MissingEmailParamsError,
     prepare_summary_email,
     send_with_sendgrid,
 )
-from rctab.settings import get_settings
 
 logger = logging.getLogger(__name__)
-
-DESIRED_RUNTIME: Final = "16:00:00"
-
-
-class FileLockContextManager(contextlib.AbstractContextManager):
-    """Create a file on enter and remove that file on exit.
-
-    Will raise an error on enter if the file already exists.
-    """
-
-    def __init__(self, filename: str) -> None:
-        """Initialise the context manager.
-
-        Args:
-            filename: Path of the file that will be created.
-        """
-        self.filename = filename
-        self.lock_file = Path(self.filename)
-
-    def __enter__(self) -> None:
-        """Try to create the lock file and raise if it already exists."""
-        self.lock_file.touch(exist_ok=False)  # raises error if file exists
-
-    def __exit__(
-        self,
-        exc_type: Optional[Type[BaseException]],
-        exc_val: Optional[BaseException],
-        exc_tb: Optional[TracebackType],
-    ) -> None:
-        """Remove the lock file on exit."""
-        logger.warning("Deleting lock file")
-        self.lock_file.unlink()
 
 
 def datetime_utcnow() -> datetime:
@@ -65,29 +27,6 @@ def datetime_utcnow() -> datetime:
     # This can be patched for testing more easily than datetime
 
     return datetime.now(timezone.utc)
-
-
-def calc_how_long_to_sleep_for(desired_runtime: str) -> float:
-    """Work out how long to sleep for to wake at the desired time.
-
-    Args:
-        desired_runtime: Desired wake time, such as "13:00:00".
-
-    Returns:
-        Seconds until daily task has to be performed.
-    """
-    # make sure all timestamps are UTC
-    right_now = datetime_utcnow()
-    date_today = right_now.strftime("%d/%m/%Y")
-    sleep_until = datetime.strptime(
-        f"{date_today} {desired_runtime}", "%d/%m/%Y %H:%M:%S"
-    ).replace(tzinfo=timezone.utc)
-    if sleep_until < right_now:
-        sleep_until += timedelta(days=1)
-    seconds_to_sleep = (sleep_until - right_now).total_seconds()
-    seconds_to_sleep = max(seconds_to_sleep, 1)
-    logger.info("Sleep for %s seconds until routine tasks", seconds_to_sleep)
-    return seconds_to_sleep
 
 
 async def send_summary_email(
@@ -157,67 +96,6 @@ async def send_summary_email(
 
     else:
         logger.error("Missing summary email recipient.")
-
-
-async def routine_tasks() -> None:
-    """An infinite loop to run routine tasks such as sending summary emails."""
-    # pylint: disable=broad-except
-    logger.info("Starting routine background tasks %s", datetime_utcnow())
-    try:
-        # Create a .lock file to make sure only one worker is running routine tasks
-        with FileLockContextManager("routine_tasks.lock"):
-            # Catch a wide array of Exceptions so that we can log them immediately
-            # rather than them being printed at server shutdown.
-            try:
-                while True:
-                    needs_to_send = True
-                    recipients = get_settings().admin_email_recipients
-                    time_last_summary_email = await get_timestamp_last_summary_email()
-                    if not recipients:
-                        # We should not send a summary email if we are testing
-                        # as there are issues with the databases library
-                        # that prohibit us from using the same databases object
-                        # in multiple async tasks if force_rollback == True
-                        # (see, amongst others, https://github.com/encode/databases/issues/456)
-                        needs_to_send = False
-                        logger.warning("No recipients for summary email found")
-
-                    elif (
-                        time_last_summary_email
-                        and time_last_summary_email.date() == datetime_utcnow().date()
-                    ):
-                        needs_to_send = False
-                        logger.info("No need to send another summary email today")
-
-                    # Check whether it's time to run the background tasks
-                    right_now = datetime_utcnow()
-                    date_today = right_now.strftime("%d/%m/%Y")
-                    scheduled_time = datetime.strptime(
-                        f"{date_today} {DESIRED_RUNTIME}", "%d/%m/%Y %H:%M:%S"
-                    ).replace(tzinfo=timezone.utc)
-                    if scheduled_time <= right_now:
-                        logger.info("Time to run background tasks")
-                        await abolish_subscriptions(UUID(ADMIN_OID))
-                        if needs_to_send:
-                            await send_summary_email(
-                                recipients, time_last_summary_email
-                            )
-                    else:
-                        logger.info(
-                            "Too early for background tasks - sleep until %s UTC",
-                            DESIRED_RUNTIME,
-                        )
-                    # We want to sleep for at least a second to avoid busy waiting
-                    seconds_to_sleep = max(
-                        1, calc_how_long_to_sleep_for(DESIRED_RUNTIME)
-                    )
-                    await sleep(seconds_to_sleep)
-
-            except BaseException:
-                logger.exception("Exception in routine_tasks")
-
-    except FileExistsError:
-        logger.exception("Exiting as we only need one routine tasks thread")
 
 
 async def get_timestamp_last_summary_email() -> Optional[datetime]:
