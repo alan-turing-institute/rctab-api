97452d094e3ef14cbe3abe8483faed8003d05ad5
Iain-S <25081046+Iain-S@users.noreply.github.com>
2023-11-23 13:41:01 +0000

    Delete tests we no longer need

--- tests/test_routes/test_daily_routine_tasks.py
+++ tests/test_routes/test_daily_routine_tasks.py
@@ -1,132 +1,24 @@
 import random
-from datetime import date, datetime, time, timedelta, timezone
+from datetime import datetime, timedelta, timezone
 from unittest.mock import AsyncMock
 from uuid import UUID
 
 import pytest
 from databases import Database
 from pytest_mock import MockerFixture
-from sqlalchemy import insert
 
 from rctab.constants import EMAIL_TYPE_SUMMARY
 from rctab.crud.accounting_models import emails, failed_emails, subscription
 from rctab.daily_routine_tasks import (
-    calc_how_long_to_sleep_for,
     get_timestamp_last_summary_email,
-    routine_tasks,
     send_summary_email,
 )
 from rctab.routers.accounting.send_emails import MissingEmailParamsError
 from tests.test_routes import constants
-from tests.test_routes.test_abolishment import create_expired_subscription
 from tests.test_routes.test_routes import (  # pylint: disable=unused-import # noqa
     create_subscription,
     test_db,
 )
-
-
-@pytest.mark.asyncio
-async def test_daily_task_loop(
-    test_db: Database,  # pylint: disable=redefined-outer-name  # noqa
-    mocker: MockerFixture,
-) -> None:
-    class BreakLoopException(Exception):
-        pass
-
-    # The summary email background task exists immediately if there are no recipients
-    mock_settings = mocker.patch("rctab.daily_routine_tasks.get_settings")
-    mock_settings.return_value.admin_email_recipients = ["me@my.org"]
-
-    mock_sleep = AsyncMock(side_effect=BreakLoopException)
-    mocker.patch("rctab.daily_routine_tasks.sleep", mock_sleep)
-
-    mock_send = AsyncMock()
-    mocker.patch("rctab.daily_routine_tasks.send_summary_email", mock_send)
-
-    mock_abolish_send = mocker.patch(
-        "rctab.routers.accounting.abolishment.send_with_sendgrid"
-    )
-    mock_abolish_send.return_value = 200
-
-    mocker.patch("rctab.daily_routine_tasks.ADMIN_OID", str(constants.ADMIN_UUID))
-
-    mock_now = mocker.patch("rctab.daily_routine_tasks.datetime_utcnow")
-    mock_now.return_value = datetime.combine(
-        date.today(), time(15, 59, 0), timezone.utc
-    )
-
-    subscription_id = await create_subscription(test_db)
-
-    await create_expired_subscription(test_db)
-
-    try:
-        await routine_tasks()
-    except BreakLoopException:
-        pass
-
-    # Since we've "woken" 10 seconds before the email is due to be sent,
-    # we don't send email yet
-    mock_send.assert_not_called()
-
-    mock_now.return_value = datetime.combine(date.today(), time(16, 1, 0), timezone.utc)
-
-    try:
-        await routine_tasks()
-    except BreakLoopException:
-        pass
-
-    # Since we've "woken" a whole 3601 seconds before the email is due to be sent,
-    # we should go back to sleep
-    mock_send.assert_called_once()
-
-    insert_statement = insert(emails).values(
-        {
-            "subscription_id": subscription_id,
-            "status": 200,
-            "type": EMAIL_TYPE_SUMMARY,
-            "recipients": ";".join(["me@mail"]),
-            # "time_created": datetime.now()
-        }
-    )
-    await test_db.execute(insert_statement)
-
-    mock_now.return_value = datetime.combine(date.today(), time(16, 2, 0), timezone.utc)
-
-    try:
-        await routine_tasks()
-    except BreakLoopException:
-        pass
-
-    # We've already sent an email on this day, so we don't want to send another one.
-    mock_send.assert_called_once()
-
-    mock_now.return_value = datetime.combine(
-        date.today(), time(16, 2, 0), timezone.utc
-    ) + timedelta(days=1)
-
-    try:
-        await routine_tasks()
-    except BreakLoopException:
-        pass
-
-    # We last sent an email yesterday so expect another
-    assert mock_send.call_count == 2
-
-
-def test_calc_how_long_to_sleep_for(mocker: MockerFixture) -> None:
-    mock_now = mocker.patch("rctab.daily_routine_tasks.datetime_utcnow")
-    mock_now.return_value = datetime.combine(date.today(), time(15, 0, 0), timezone.utc)
-    assert calc_how_long_to_sleep_for("16:00:00") == 3600
-    assert calc_how_long_to_sleep_for("14:00:00") == 3600 * 23
-
-
-def test_calc_how_long_to_sleep_floor(mocker: MockerFixture) -> None:
-    mock_now = mocker.patch("rctab.daily_routine_tasks.datetime_utcnow")
-    mock_now.return_value = datetime.combine(
-        date.today(), time(11, 59, 59, 500000), timezone.utc
-    )
-    #  We never want to sleep for less than one second
-    assert calc_how_long_to_sleep_for("12:00:00") == 1
 
 
 @pytest.mark.asyncio
