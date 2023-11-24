7bdcb7356d6af51151905ff70c2521293d7a5249
Iain-S <25081046+Iain-S@users.noreply.github.com>
2023-11-24 16:58:26 +0000

    Remove celery log-to-file option

--- scripts/prestart.sh
+++ scripts/prestart.sh
@@ -3,8 +3,8 @@
 
 # For background tasks, we need Redis and Celery running
 redis-server ./redis.conf &
-celery -A rctab.tasks worker --detach --concurrency 1 --loglevel=info --logfile=celery.log
-celery -A rctab.tasks beat --detach --loglevel=info --logfile=beat.log
+celery -A rctab.tasks worker --detach --concurrency 1 --loglevel=info
+celery -A rctab.tasks beat --detach --loglevel=info
 
 # Give Postgres, Redis and Celery a few seconds to start
 sleep 5
